37d599c414f2f3b58160532a974bfa8cf6672781
Blahodarna Oksana <blagodarnaoksana@ukr.net>
2025-11-13 22:36:06 +0200

    added pop_back()

--- main.cpp
+++ main.cpp
@@ -4,9 +4,9 @@ template <typename T>
 class MyVector
 {
 private:
-    T *data;    // Указатель на массив
-    int size;   // сколько реально элементов мы положили.
-    int volume; // сколько памяти выделено
+    T *data;      // Указатель на массив
+    int size;     // сколько реально элементов мы положили.
+    int capacity; // сколько памяти выделено
 
 public:
     // Конструктор
@@ -14,7 +14,7 @@ public:
     {
         data = nullptr;
         size = 0;
-        volume = 0;
+        capacity = 0;
     }
 
     // Деструктор
@@ -29,12 +29,12 @@ public:
     // Добавление элемента
     void push_back(const T &value)
     {
-        // volume всегда >= size. Когда массив заполнен, мы удваиваем volume, чтобы потом добавлять элементы без частых копирований.
-        if (size >= volume)
+        // capacity всегда >= size. Когда массив заполнен, мы удваиваем capacity, чтобы потом добавлять элементы без частых копирований.
+        if (size >= capacity)
         {
             // нужно больше памяти
-            int newVolume = (volume == 0) ? 1 : volume * 2;
-            T *newData = new T[newVolume];
+            int newCapacity = (capacity == 0) ? 1 : capacity * 2;
+            T *newData = new T[newCapacity];
 
             // копируем старые данные
             for (int i = 0; i < size; ++i)
@@ -42,11 +42,23 @@ public:
 
             delete[] data; // освобождаем старую память
             data = newData;
-            volume = newVolume;
+            capacity = newCapacity;
         }
 
         data[size] = value;
         ++size;
+    }
+
+    void pop_back()
+    {
+        // Размер контейнера (size()) уменьшается на 1.
+        // Ёмкость (capacity()) не изменяется.
+        // Сам элемент просто уничтожается (вызов его деструктора).
+        if (size == 0)
+            return;
+
+        --size;          // уменьшаем размер
+        data[size].~T(); // вызываем деструктор последнего элемента
     }
 
     // Получение элемента по индексу
@@ -61,9 +73,9 @@ public:
         return size;
     }
 
-    int getVolume()
+    int getCapacity()
     {
-        return volume;
+        return capacity;
     }
 };
 
@@ -79,25 +91,22 @@ int main()
     vi.push_back(7);
     vi.push_back(8);
 
-    MyVector<double> vd; // вектор для double
-    vd.push_back(3.14);
-    vd.push_back(2.71);
-
-    MyVector<std::string> vs; // вектор для строк
-    vs.push_back("Hello");
-    vs.push_back("World");
-    vs.push_back("this");
-    vs.push_back("is");
-    vs.push_back("string");
+    vi.pop_back();
+    vi.pop_back();
 
     // Integer
     std::cout << "---------Integer-------------" << std::endl;
     for (int i = 0; i < vi.getSize(); ++i)
-        std::cout << vi[i] << ", ";
+        std::cout << vi[i] << " ";
 
     std::cout << std::endl;
     std::cout << "Integer Size arr: " << vi.getSize() << std::endl;
-    std::cout << "Integer volume: " << vi.getVolume() << std::endl;
+    std::cout << "Integer capacity: " << vi.getCapacity() << std::endl;
+
+    /*
+    MyVector<double> vd; // вектор для double
+    vd.push_back(3.14);
+    vd.push_back(2.71);
 
     // Double
     std::cout << "----------Double------------" << std::endl;
@@ -106,7 +115,14 @@ int main()
 
     std::cout << std::endl;
     std::cout << "Double Size arr: " << vd.getSize() << std::endl;
-    std::cout << "Double volume: " << vd.getVolume() << std::endl;
+    std::cout << "Double capacity: " << vd.getCapacity() << std::endl;
+
+    MyVector<std::string> vs; // вектор для строк
+    vs.push_back("Hello");
+    vs.push_back("World");
+    vs.push_back("this");
+    vs.push_back("is");
+    vs.push_back("string");
 
     // String
     std::cout << "----------String------------" << std::endl;
@@ -115,7 +131,7 @@ int main()
 
     std::cout << std::endl;
     std::cout << "String Size arr: " << vs.getSize() << std::endl;
-    std::cout << "String volume: " << vs.getVolume() << std::endl;
-
+    std::cout << "String capacity: " << vs.getCapacity() << std::endl;
+    */
     return 0;
 }

68997e9dc1d966bb6610d8a4b9d86b75988c1a20
Blahodarna Oksana <blagodarnaoksana@ukr.net>
2025-11-14 16:02:08 +0200

    added empty, clear, reserve

--- main.cpp
+++ main.cpp
@@ -30,35 +30,62 @@ public:
     void push_back(const T &value)
     {
         // capacity всегда >= size. Когда массив заполнен, мы удваиваем capacity, чтобы потом добавлять элементы без частых копирований.
-        if (size >= capacity)
+        if (size == capacity)
         {
-            // нужно больше памяти
+            // 2. Рассчитываем новую емкость
             int newCapacity = (capacity == 0) ? 1 : capacity * 2;
-            T *newData = new T[newCapacity];
 
-            // копируем старые данные
-            for (int i = 0; i < size; ++i)
-                newData[i] = data[i];
-
-            delete[] data; // освобождаем старую память
-            data = newData;
-            capacity = newCapacity;
+            // 3. Вызываем reserve() для выделения новой памяти и копирования данных
+            reserve(newCapacity);
         }
 
         data[size] = value;
         ++size;
     }
 
+    void reserve(int newCapacity)
+    {
+        if (newCapacity <= capacity)
+            return;
+
+        T *newData = new T[newCapacity];
+
+        for (int i = 0; i < size; ++i)
+            newData[i] = data[i];
+
+        delete[] data;
+        data = newData;
+        capacity = newCapacity;
+    }
+
     void pop_back()
     {
-        // Размер контейнера (size()) уменьшается на 1.
-        // Ёмкость (capacity()) не изменяется.
+        // Размер size уменьшается на 1.
+        // Ёмкость capacity не изменяется.
         // Сам элемент просто уничтожается (вызов его деструктора).
         if (size == 0)
             return;
 
         --size;          // уменьшаем размер
         data[size].~T(); // вызываем деструктор последнего элемента
+    }
+
+    bool empty()
+    {
+        return (size == 0) ? true : false;
+    }
+
+    void clear()
+    {
+        // Устанавливает size = 0
+        // Ёмкость capacity не изменяется.
+        // Вызываем деструктор
+        for (int i = 0; i < size; ++i)
+        {
+            data[i].~T();
+        }
+
+        size = 0;
     }
 
     // Получение элемента по индексу
@@ -94,6 +121,8 @@ int main()
     vi.pop_back();
     vi.pop_back();
 
+    // vi.clear();
+
     // Integer
     std::cout << "---------Integer-------------" << std::endl;
     for (int i = 0; i < vi.getSize(); ++i)
@@ -102,6 +131,7 @@ int main()
     std::cout << std::endl;
     std::cout << "Integer Size arr: " << vi.getSize() << std::endl;
     std::cout << "Integer capacity: " << vi.getCapacity() << std::endl;
+    std::cout << "isEmpty         : " << vi.empty() << std::endl;
 
     /*
     MyVector<double> vd; // вектор для double
@@ -116,22 +146,34 @@ int main()
     std::cout << std::endl;
     std::cout << "Double Size arr: " << vd.getSize() << std::endl;
     std::cout << "Double capacity: " << vd.getCapacity() << std::endl;
-
-    MyVector<std::string> vs; // вектор для строк
-    vs.push_back("Hello");
-    vs.push_back("World");
-    vs.push_back("this");
-    vs.push_back("is");
-    vs.push_back("string");
-
-    // String
-    std::cout << "----------String------------" << std::endl;
-    for (int i = 0; i < vs.getSize(); ++i)
-        std::cout << vs[i] << ", ";
-
-    std::cout << std::endl;
-    std::cout << "String Size arr: " << vs.getSize() << std::endl;
-    std::cout << "String capacity: " << vs.getCapacity() << std::endl;
     */
+
+    /*
+     MyVector<std::string> vs; // вектор для строк
+     vs.push_back("Hello");
+     vs.push_back("World");
+     vs.push_back("this");
+     vs.push_back("is");
+     vs.push_back("string");
+
+     // String
+     std::cout << "----------String------------" << std::endl;
+     for (int i = 0; i < vs.getSize(); ++i)
+         std::cout << vs[i] << ", ";
+
+     std::cout << std::endl;
+     std::cout << "String Size arr: " << vs.getSize() << std::endl;
+     std::cout << "String capacity: " << vs.getCapacity() << std::endl;
+     */
+
+    /*
+      INFORMATION:
+      reserve() вызывается не напрямую пользователем, а внутри метода push_back()
+      Пользователь вызывает vec.push_back(value)
+      push_back() видит, что size == capacity (память закончилась)
+      Вектор увеличивает свою ёмкость (capacity), обычно удваивая её, чтобы обеспечить быстрые вставки в будущем
+      Цель reserve() — минимизировать дорогие операции. Перераспределение и копирование памяти — это медленные операции.
+    */
+
     return 0;
 }
